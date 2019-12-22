6bdd631c133a211b91f83ee17d41ea73bb6799f9
Andrius Vaitiekunas <waitiekunas@gmail.com>
2019-12-22 17:23:41 +0200

    removed redux devtools

--- src/state/createStore.tsx
+++ src/state/createStore.tsx
@@ -1,10 +1,10 @@
 import { createStore } from 'redux';
 import { allReducers } from '../state/reducers';
-import initialState from './initialState'
 
 
 // preloadedState will be passed in by the plugin
 export default store => {
     return createStore(allReducers,
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+    );
 };
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
